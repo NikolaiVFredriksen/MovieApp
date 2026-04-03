322907b62d5684cbac0b78cf1847c3e8a2b97df1
Nikolai V. Fredriksen <cooprix1@gmail.com>
2026-04-03 11:40:19 +0200

    fix test

--- src/appwrite.js
+++ src/appwrite.js
@@ -14,9 +14,8 @@ export const account = new Account(client);
 // Google login
 export const loginWithGoogle = () => {
   const redirectUrl =
-    import.meta.env.VITE_REDIRECT_URL ||
-    "https://movie-app-git-oscars-companion-zillacoops-projects.vercel.app/";
-  console.log("redirectUrl:", redirectUrl);
+    import.meta.env.VITE_REDIRECT_URL || "http://localhost:5173";
+  console.log("REDIRECT URL:", redirectUrl);
   account.createOAuth2Session("google", redirectUrl, redirectUrl, []);
 };
 
